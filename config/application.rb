c9dd75e499f06b20fc0512c91f7b98b9fab10c3f
Dee Ragoo <deeskii32443@gmail.com>
2021-03-22 00:08:48 -0400

    rolling back

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
+USERNAME = ENV['USERNAME']
 module Demo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

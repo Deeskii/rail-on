9efa8e5d1e404343fff758da44a6d5ca560c3e40
Dee Ragoo <deeskii32443@gmail.com>
2021-03-22 01:26:05 -0400

    created pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get '/users', to: 'users#index'
-
-  root 'welcome#index'
+  get '/home/about'
+  get 'projects', to: 'projects#index'
+  root 'home#index'
 end
